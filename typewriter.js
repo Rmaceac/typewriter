6012fd13d924bf17ed34b186e2fc64aa73796e9b
Ryan <ryan.maceachern@gmail.com>
2022-02-15 13:05:25 -0800

    completed typewriter function

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,17 @@
-say a bunch of stuff
+const sentence = "top of the morning to you gentleperson!";
+
+
+const timedPrint = function() {
+  let duration = 0;
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, duration);
+    duration += 50;
+  }
+  setTimeout(() => {
+    process.stdout.write("\n");
+  }, duration);
+};
+
+timedPrint();
